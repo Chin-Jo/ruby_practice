07255fc7cf007d99e0af5eb32ed6f1e7bb7a44ae
Chin-Jo <jo.chin@divx.co.jp>
2024-06-06 17:57:56 +0900

    day 6/6 finish study of firstApp

--- firstApp/first_app/config/routes.rb
+++ firstApp/first_app/config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   # get "up" => "rails/health#show", as: :rails_health_check
   get 'posts', to: 'posts#index'
-
-
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
   # Defines the root path route ("/")
   # root "posts#index"
 end
